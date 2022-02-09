901adf68400a5ad9830952a50dc4b208e08e08f4
wesleyjrs <wesley.jr17@outlook.com>
2022-02-09 10:20:45 -0300

    Croação do projeto treinamento java JDEV.

---
+++ first-project-jdev/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,11 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Meu primeiro projeto do curso JDEV");
+		
+	}
+
+}
