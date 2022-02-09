33fd5d8af87fbc2aa206c6ead6b5c9a9a0394ac9
wesleyjrs <wesley.jr17@outlook.com>
2022-02-09 11:03:28 -0300

    adicionado data de nascimento na classe pessoa.

--- first-project-jdev/src/cursojava/jdev/Pessoa.java
+++ first-project-jdev/src/cursojava/jdev/Pessoa.java
@@ -1,23 +1,27 @@
 package cursojava.jdev;
 
+import java.util.Date;
+
 public class Pessoa {
 	
 	private String nome;
 	private String sobrenome;
 	private Integer idade;
 	private String sexo;
+	private Date dtNascimento;
 	
 	public Pessoa() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Pessoa(String nome, String sobrenome, Integer idade, String sexo) {
+	public Pessoa(String nome, String sobrenome, Integer idade, String sexo, Date dtNascimento) {
 		super();
 		this.nome = nome;
 		this.sobrenome = sobrenome;
 		this.idade = idade;
 		this.sexo = sexo;
+		this.dtNascimento = dtNascimento;
 	}
 
 	public String getNome() {
@@ -51,6 +55,16 @@ public class Pessoa {
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
+
+	public Date getDtNascimento() {
+		return dtNascimento;
+	}
+
+	public void setDtNascimento(Date dtNascimento) {
+		this.dtNascimento = dtNascimento;
+	}
+	
+	
 	
 	
 }
