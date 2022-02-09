862eb17df3ff12baef588c0bccc5eebae1f395c0
wesleyjrs <wesley.jr17@outlook.com>
2022-02-09 10:29:08 -0300

    adicionado nova mensagem subtitulo.

--- first-project-jdev/src/cursojava/executavel/PrimeiraClasseJava.java
+++ first-project-jdev/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,6 +5,7 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		
 		System.out.println("Meu primeiro projeto do curso JDEV");
+		System.out.println("orientação a objetos");
 		
 	}
 
